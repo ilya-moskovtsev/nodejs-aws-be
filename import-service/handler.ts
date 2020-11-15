da7a6362c047d11cc7e79945750855bf978c45d1
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2020-11-15 20:11:16 +0300

    add importProductsFile

--- import-service/handler.ts
+++ import-service/handler.ts
@@ -1,15 +1,3 @@
-import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import {importProductsFile} from './handlers/importProductsFile';
 
-export async function hello(
-  event: APIGatewayEvent,
-  context: Context
-): Promise<APIGatewayProxyResult> {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v2.0! Your function executed successfully!",
-      context,
-      event,
-    }),
-  };
-}
+export {importProductsFile};
