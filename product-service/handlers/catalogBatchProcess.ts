2c6a5196e007dbd021663a670ef596c4c55c7f83
Ilya Moskovtsev <ilya.moskovtsev@gmail.com>
2020-11-24 12:03:53 +0300

    fix catalogBatchProcess

--- product-service/handlers/catalogBatchProcess.ts
+++ product-service/handlers/catalogBatchProcess.ts
@@ -1,6 +1,5 @@
 import {Context, SQSEvent} from "aws-lambda";
 import AWS from "aws-sdk";
-import {corsHeaders} from "../../import-service/constants/headers";
 import {productRules} from "../validation/ProductRules";
 import Validator from 'validatorjs';
 import {Client} from "pg";
@@ -17,78 +16,82 @@ const dbOptions = {
     connectionTimeoutMillis: 5000
 };
 
+const sns = new AWS.SNS();
+
 export async function catalogBatchProcess(
     event: SQSEvent,
     context: Context
 ) {
     console.log('event', event);
-    const products = event.Records.map(({body}) => body);
-    const sns = new AWS.SNS();
+    try {
+        const client = new Client(dbOptions);
+        await Promise.all(
+            event.Records.map(async (record) => {
+                try {
+                    const parsedProduct = JSON.parse(record.body);
+                    const validation = new Validator(parsedProduct, productRules);
+                    if (validation.fails()) {
+                        throw new Error(
+                            `validation of ${JSON.stringify(
+                                parsedProduct
+                            )} failed with errors: ${JSON.stringify(validation.errors.all())}`
+                        );
+                    }
 
-    console.log('products', products);
-    const promises = products.map(async (product) => {
-        const parsedProduct = JSON.parse(product);
-        const validation = new Validator(parsedProduct, productRules);
+                    console.log(`adding product ${JSON.stringify(parsedProduct)} to db`);
+                    const {
+                        count,
+                        description,
+                        id,
+                        price,
+                        title,
+                        image_url,
+                        image_title
+                    } = parsedProduct;
+                    try {
+                        await client.connect();
 
-        if (validation.fails()) {
-            const errorMessage = `Product not valid`;
-            const errors = validation.errors.all()
-            console.log(errorMessage, errors)
-        } else {
-            const {
-                count,
-                description,
-                id,
-                price,
-                title,
-                image_url,
-                image_title
-            } = parsedProduct;
+                        await client.query('BEGIN');
 
-            const client = new Client(dbOptions);
-            try {
-                await client.connect();
+                        const text = `insert into products (id, title, description, price, image_url, image_title)
+                                      values ($1, $2, $3, $4, $5, $6)
+                                      returning id;`;
+                        const values = [id || uuidv4(), title, description, price, image_url, image_title];
+                        const product = await client.query(text, values);
 
-                await client.query('BEGIN');
+                        await client.query(`
+                            insert into stocks (product_id, count)
+                            values ('${product.rows[0].id}', ${count});
+                        `);
 
-                const text = `insert into products (id, title, description, price, image_url, image_title)
-                              values ($1, $2, $3, $4, $5, $6)
-                              returning id;`;
-                const values = [id || uuidv4(), title, description, price, image_url, image_title];
-                const product = await client.query(text, values);
+                        await client.query('COMMIT');
+                    } catch (err) {
+                        console.error('Error during database request execution:', err);
+                        await client.query('ROLLBACK');
+                    } finally {
+                        client.end();
+                    }
 
-                await client.query(`
-            insert into stocks (product_id, count)
-            values ('${product.rows[0].id}', ${count});
-        `);
 
-                await client.query('COMMIT');
-            } catch (err) {
-                console.error('Error during database request execution:', err);
-                await client.query('ROLLBACK');
-            } finally {
-                client.end();
-            }
-        }
-    });
+                    return sns.publish({
+                        Message: `product was created: ${JSON.stringify(parsedProduct)}`,
+                        TopicArn: SNS_TOPIC_ARN,
+                        MessageAttributes: {
+                            count: {
+                                DataType: 'Number',
+                                StringValue: parsedProduct.count,
+                            },
+                        },
+                    });
+                } catch (error) {
+                    console.log('error', error);
+                    throw error;
+                }
+            })
+        );
 
-    await Promise.all(promises);
-
-    sns.publish({
-        Subject: 'Products added',
-        Message: JSON.stringify(products),
-        TopicArn: SNS_TOPIC_ARN
-    }, (err, data) => {
-        if (err) {
-            console.error('Error while publishing to topic', err);
-        } else {
-            console.log('Successfully published to topic', data);
-        }
-    });
-
-    return {
-        statusCode: 202,
-        headers: corsHeaders,
-        body: '',
-    };
+    } catch (e) {
+        console.log('error', e);
+        throw e;
+    }
 }
