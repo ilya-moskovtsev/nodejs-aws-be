7780429d4269e0fe38d974110728f55664235fd7
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2020-11-28 16:39:04 +0300

    check type: "TOKEN"

--- authorization-service/handlers/basicAuthorizer.ts
+++ authorization-service/handlers/basicAuthorizer.ts
@@ -1,13 +1,21 @@
-import {APIGatewayEvent, APIGatewayProxyResult, Context} from "aws-lambda";
+import {APIGatewayAuthorizerEvent, APIGatewayProxyResult, Context} from "aws-lambda";
 import {corsHeaders} from "../constants/headers";
 
 export async function basicAuthorizer(
-    event: APIGatewayEvent,
+    event: APIGatewayAuthorizerEvent,
     context: Context
 ): Promise<APIGatewayProxyResult> {
     console.log('event', event);
-    console.log('event.body', event.body);
     console.log(process.env.ilya_moskovtsev);
+
+    if (event['type'] !== 'TOKEN') {
+        console.log('401 Unauthorized');
+        return {
+            statusCode: 401,
+            headers: corsHeaders,
+            body: 'Unauthorized',
+        };
+    }
 
     return {
         statusCode: 200,
--- authorization-service/tests/handlers/basicAuthorizer.test.ts
+++ authorization-service/tests/handlers/basicAuthorizer.test.ts
@@ -1,12 +1,31 @@
-import {APIGatewayEvent, Context} from "aws-lambda";
+import {APIGatewayAuthorizerEvent, Context} from "aws-lambda";
 import * as handler from "../../handler";
 
 test("basicAuthorizer", async () => {
-    const event = {body: "Test Body"} as APIGatewayEvent;
+    const event = {
+        type: "TOKEN",
+        methodArn: 'arn',
+        authorizationToken: 'test'
+    } as APIGatewayAuthorizerEvent;
     const context = {} as Context;
 
     const response = await handler.basicAuthorizer(event, context);
 
     expect(response.statusCode).toEqual(200);
     expect(typeof response.body).toBe("string");
+});
+
+test("401 Unauthorized", async () => {
+    // @ts-ignore
+    const event = {
+        type: "NOT_TOKEN",
+        methodArn: 'arn',
+        authorizationToken: 'test'
+    } as APIGatewayAuthorizerEvent;
+    const context = {} as Context;
+
+    const response = await handler.basicAuthorizer(event, context);
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body).toBe('Unauthorized');
 });
