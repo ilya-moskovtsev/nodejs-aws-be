497c554007dee41ed051865bb5203044cd249fd5
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2020-11-28 19:54:54 +0300

    update basicAuthorizer

--- authorization-service/handlers/basicAuthorizer.ts
+++ authorization-service/handlers/basicAuthorizer.ts
@@ -16,10 +16,54 @@ export async function basicAuthorizer(
             body: 'Unauthorized',
         };
     }
+    try {
+        const authorizationToken = event.authorizationToken;
 
+        const encodedCredentials = authorizationToken.split(' ')[1];
+        const buffer = Buffer.from(encodedCredentials, 'base64');
+        const [username, password] = buffer.toString('utf8').split(':');
+        console.log(`username: ${username} and password: ${password}`);
+
+        const storedUsername = process.env[username];
+        const effect = !storedUsername || storedUsername !== password ? 'Deny' : 'Allow';
+
+        if (effect === 'Deny') {
+            return {
+                statusCode: 403,
+                headers: corsHeaders,
+                body: `Unauthorized`,
+            };
+        }
+
+        const policy = generatePolicy(encodedCredentials, event.methodArn, effect);
+
+        return {
+            statusCode: 200,
+            headers: corsHeaders,
+            body: JSON.stringify(policy),
+        };
+    } catch (e) {
+        return {
+            statusCode: 401,
+            headers: corsHeaders,
+            body: `Unauthorized: ${e.message}`,
+        };
+    }
+
+}
+
+function generatePolicy(encodedCredentials: string, methodArn: string, effect: string = 'Deny') {
     return {
-        statusCode: 200,
-        headers: corsHeaders,
-        body: 'body',
-    };
+        principalId: encodedCredentials,
+        policyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Action: 'execute-api:Invoke',
+                    Effect: effect,
+                    Resource: methodArn
+                }
+            ]
+        }
+    }
 }
--- authorization-service/tests/handlers/basicAuthorizer.test.ts
+++ authorization-service/tests/handlers/basicAuthorizer.test.ts
@@ -1,31 +1,82 @@
 import {APIGatewayAuthorizerEvent, Context} from "aws-lambda";
 import * as handler from "../../handler";
 
-test("basicAuthorizer", async () => {
-    const event = {
-        type: "TOKEN",
-        methodArn: 'arn',
-        authorizationToken: 'test'
-    } as APIGatewayAuthorizerEvent;
-    const context = {} as Context;
+describe('basicAuthorizer', () => {
+    const OLD_ENV = process.env;
 
-    const response = await handler.basicAuthorizer(event, context);
+    beforeEach(() => {
+        jest.resetModules();
+        process.env = {...OLD_ENV};
+    });
 
-    expect(response.statusCode).toEqual(200);
-    expect(typeof response.body).toBe("string");
-});
+    afterAll(() => {
+        process.env = OLD_ENV;
+    });
 
-test("401 Unauthorized", async () => {
-    // @ts-ignore
-    const event = {
-        type: "NOT_TOKEN",
-        methodArn: 'arn',
-        authorizationToken: 'test'
-    } as APIGatewayAuthorizerEvent;
-    const context = {} as Context;
+    test("success", async () => {
+        const username = 'username';
+        const password = 'password';
 
-    const response = await handler.basicAuthorizer(event, context);
+        process.env[username] = password;
+        const testedModule = require('../../handler').basicAuthorizer;
 
-    expect(response.statusCode).toEqual(401);
-    expect(response.body).toBe('Unauthorized');
+        const plainCredentials = `${username}:${password}`;
+        const buffer = Buffer.from(plainCredentials, 'utf-8');
+        const encodedCredentials = buffer.toString('base64');
+
+        const event = {
+            type: "TOKEN",
+            methodArn: 'arn',
+            authorizationToken: `Basic ${encodedCredentials}`
+        } as APIGatewayAuthorizerEvent;
+        const context = {} as Context;
+
+        const response = await testedModule(event, context);
+
+        expect(response.statusCode).toEqual(200);
+        expect(typeof response.body).toBe("string");
+        expect(response.body).toBe(JSON.stringify({
+            principalId: encodedCredentials,
+            policyDocument: {
+                Version: '2012-10-17',
+                Statement: [
+                    {
+                        Action: 'execute-api:Invoke',
+                        Effect: 'Allow',
+                        Resource: 'arn'
+                    }
+                ]
+            }
+        }));
+    });
+
+    test("when not token than 401 Unauthorized", async () => {
+        // @ts-ignore
+        const event = {
+            type: "NOT_TOKEN",
+            methodArn: 'arn',
+            authorizationToken: 'test'
+        } as APIGatewayAuthorizerEvent;
+        const context = {} as Context;
+
+        const response = await handler.basicAuthorizer(event, context);
+
+        expect(response.statusCode).toEqual(401);
+        expect(response.body).toBe('Unauthorized');
+    });
+
+    test("when wrong credentials than 403 Unauthorized", async () => {
+        // @ts-ignore
+        const event = {
+            type: "TOKEN",
+            methodArn: 'arn',
+            authorizationToken: 'Basic wrong_credentials'
+        } as APIGatewayAuthorizerEvent;
+        const context = {} as Context;
+
+        const response = await handler.basicAuthorizer(event, context);
+
+        expect(response.statusCode).toEqual(403);
+        expect(response.body).toBe('Unauthorized');
+    });
 });
