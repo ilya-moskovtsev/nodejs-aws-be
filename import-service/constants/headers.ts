50685bb90cb27f49a97c9200ca24ec8e1f3ef2f8
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2020-11-28 21:55:28 +0300

    update headers

--- import-service/constants/headers.ts
+++ import-service/constants/headers.ts
@@ -1,5 +1,6 @@
 export const corsHeaders = {
     "Access-Control-Allow-Headers": "Content-Type",
     "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
     "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
 }
